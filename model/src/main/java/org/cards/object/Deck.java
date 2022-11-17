916de696975f9bdb1fc1a334a559e687e7e81cd5
lursz <lukoszr@gmail.com>
2022-11-17 20:12:45 +0100

    added Deck

--- model/src/main/java/org/cards/object/Deck.java
+++ model/src/main/java/org/cards/object/Deck.java
@@ -1,4 +1,72 @@
 package org.cards.object;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ * Class describing a deck of cards
+ * @param cards - list of cards
+ * @param cardsLeft - number of cards left in the deck
+ * @param size_ - size of the deck
+ */
 public class Deck {
+    private ArrayList<Card> deck_;
+    private int size_;
+    private int cardsLeft_;
+
+    // Constructor
+    public Deck() {
+        deck_ = new ArrayList<Card>();
+        this.size_ = 52;
+        this.cardsLeft_ = 52;
+        for (Card.suits iSuit : Card.suits.values()) {
+            for (Card.ranks iRank : Card.ranks.values()) {
+                deck_.add(new Card(iSuit, iRank));
+            }
+        }
+
+    }
+
+    // Methods
+    public Deck fabryki(Deck tempDeck) {
+        tempDeck.deck_.clear();
+        for (Card.suits iSuit : Card.suits.values()) {
+            for (Card.ranks iRank : Card.ranks.values()) {
+                deck_.add(new Card(iSuit, iRank));
+            }
+        }
+        return tempDeck;
+    }
+
+    public Deck shuffle(Deck tempDeck) {
+        Collections.shuffle(tempDeck.deck_);
+        return tempDeck;
+    }
+
+    public Card draw() {
+        if (cardsLeft_ > 0) {
+            Card tempCard = deck_.get(0);
+            deck_.remove(0);
+            cardsLeft_--;
+            return tempCard;
+        } else {
+            return null;
+        }
+    }
+
+    public void printDeck() {
+        for (Card i : deck_) {
+            System.out.println(i);
+        }
+    }
+
+    public int getSize_() {
+        return size_;
+    }
+
+    public int getCardsLeft_() {
+        return cardsLeft_;
+    }
+
 }
