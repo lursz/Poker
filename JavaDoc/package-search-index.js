73f11e57c12a114149711c9823bef25a7f2c73b6
lursz <93160829+lursz@users.noreply.github.com>
2023-01-18 00:25:25 +0100

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.cards"},{"l":"org.cards.exceptions"},{"l":"org.cards.game"},{"l":"org.cards.object"},{"l":"org.cards.player"}];updateSearchResults();
